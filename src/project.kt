138b6ad02b30ccdcb62ab78c801be8ad0ae245ed
LIZA <liza.miminoshvili.1@btu.edu.ge>
2022-07-02 03:46:41 +0400

    Initial commit

---
+++ src/project.kt
@@ -0,0 +1,27 @@
+//#1 a programme to check if a word is palindrome or not
+//fun main(){
+//    examples:
+//    println("Phrase: \"madam\"")
+//    println("isPalindrome : ${isPalindrome("madam")}")
+//    println("Phrase \"Elizabeth\"")
+//    println("isPalindrome : ${isPalindrome("Elizabeth")}")
+//    println("Phrase: \"a\"")
+//    println(isPalindrome : ${isPalindrome("a")}")
+//
+//}
+//fun isPalindrome(text: String): Boolean{
+//    val textLen = text.length
+
+//    the condition above is used for words with only 1 character
+//    if(textLen < 2){
+//        return true
+//    }
+//    checking first and last characters and then I use recursion to check all other characters one by one
+//    return text.first() == text.last()
+//            && isPalindrome(text.substring(1, textLen - 1))
+//}
+
+//can't write other exercises because I only have an OOP knowledge in Kotlin and can work only in Android Studio, I would be pleased if you took a look at my projects in GitHub
+
+
+
